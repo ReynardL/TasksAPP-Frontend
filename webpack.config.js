2ab9d9727b26237e0705d84bea6700494ddd0411
ReynardL <163877889+ReynardL@users.noreply.github.com>
2025-01-30 10:49:04 -0500

    Removed firebase autodeploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require('dotenv-webpack');
-const webpack = require('webpack');
 
 module.exports = {
     entry: "./src/index.js",
@@ -35,9 +34,6 @@ module.exports = {
         new Dotenv(),
         new HtmlWebpackPlugin({
             template: "./public/index.html",
-        }),
-        new webpack.DefinePlugin({
-            'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
         }),
     ],
     devServer: {
